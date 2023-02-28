034f0a4428f0d04bda3c2163d31eb3a776eb368a
HzRojo <123857255+HzRojo@users.noreply.github.com>
2023-02-28 13:00:19 -0300

    Este es mi primer commit

---
+++ Emanuel Luque - Proyecto CV - Argentina Programa 4.0/js/function.js
@@ -0,0 +1,77 @@
+function mostrarInfo() {
+    document.getElementById('infoper-cv').style.display = 'block';
+    document.getElementById('btninfoM').style.display = 'none';
+    document.getElementById('btninfoO').style.display = 'block';
+}
+
+function ocultarInfo() {
+    document.getElementById('infoper-cv').style.display = 'none';
+    document.getElementById('btninfoO').style.display = 'none';
+    document.getElementById('btninfoM').style.display = 'block';
+    
+}
+
+function mostrarExp() {
+    document.getElementById('explab-cv').style.display = 'block';
+    document.getElementById('btnexpM').style.display = 'none';
+    document.getElementById('btnexpO').style.display = 'block';
+}
+
+function ocultarExp() {
+    document.getElementById('explab-cv').style.display = 'none';
+    document.getElementById('btnexpO').style.display = 'none';
+    document.getElementById('btnexpM').style.display = 'block';
+    
+}
+
+function mostrarLen() {
+    document.getElementById('len-cv').style.display = 'block';
+    document.getElementById('btnlenM').style.display = 'none';
+    document.getElementById('btnlenO').style.display = 'block';
+}
+
+function ocultarLen() {
+    document.getElementById('len-cv').style.display = 'none';
+    document.getElementById('btnlenO').style.display = 'none';
+    document.getElementById('btnlenM').style.display = 'block';
+    
+}
+
+function mostrarEdu() {
+    document.getElementById('edu-cv').style.display = 'block';
+    document.getElementById('btneduM').style.display = 'none';
+    document.getElementById('btneduO').style.display = 'block';
+}
+
+function ocultarEdu() {
+    document.getElementById('edu-cv').style.display = 'none';
+    document.getElementById('btneduO').style.display = 'none';
+    document.getElementById('btneduM').style.display = 'block';
+    
+}
+
+function mostrarHab() {
+    document.getElementById('hab-cv').style.display = 'block';
+    document.getElementById('btnhabM').style.display = 'none';
+    document.getElementById('btnhabO').style.display = 'block';
+}
+
+function ocultarHab() {
+    document.getElementById('hab-cv').style.display = 'none';
+    document.getElementById('btnhabO').style.display = 'none';
+    document.getElementById('btnhabM').style.display = 'block';
+    
+}
+
+function mostrarCont() {
+    document.getElementById('cont-cv').style.display = 'flex';
+    document.getElementById('btncontM').style.display = 'none';
+    document.getElementById('btncontO').style.display = 'block';
+}
+
+function ocultarCont() {
+    document.getElementById('cont-cv').style.display = 'none';
+    document.getElementById('btncontO').style.display = 'none';
+    document.getElementById('btncontM').style.display = 'block';
+    
+}
